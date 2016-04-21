78057774a68e6898f6479ffb5e6b2f14161c4baa
swihart <bruce.swihart@gmail.com>
2016-04-21 13:46:36 -0400

    this branch's commits implement the log and log.p features

--- tests/testthat/test_v01.R
+++ tests/testthat/test_v01.R
@@ -24,3 +24,32 @@ test_that("Confirm q(p(x)) = x", {
                tolerance = 1e-6,
                scale = 1)
 })
+
+
+test_that("Confirm log = TRUE feature works in dbridge", {
+  phi <- runif(1, 0.01, 0.99)
+  x <- rnorm(1, sd=2)
+  expect_equal(object =       dbridge(x, scale=phi, log=TRUE),
+               expected = log(dbridge(x, scale=phi, log=FALSE)),
+               tolerance = 1e-6,
+               scale = 1)
+})
+
+
+test_that("Confirm log.p = TRUE feature works in pbridge", {
+  phi <- runif(1, 0.01, 0.99)
+  x <- rnorm(1, sd=2)
+  expect_equal(object =       pbridge(x, scale=phi, log.p=TRUE),
+               expected = log(pbridge(x, scale=phi, log.p=FALSE)),
+               tolerance = 1e-6,
+               scale = 1)
+})
+
+test_that("Confirm log.p = TRUE feature works in qbridge", {
+  phi <- runif(1, 0.01, 0.99)
+  x <- runif(1, 0.01, 0.99)
+  expect_equal(object =       qbridge(log(x), scale=phi, log.p=TRUE),
+               expected =     qbridge(    x , scale=phi, log.p=FALSE),
+               tolerance = 1e-6,
+               scale = 1)
+})
